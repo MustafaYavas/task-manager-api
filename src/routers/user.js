aff0deb46b9cb3496d16a1b133cdf6502add1e7f
UB4ME <mustafayavas40@gmail.com>
2021-03-06 07:40:34 +0100

    error messages modified

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,7 +15,7 @@ router.post("/users", async (req, res) => {
         const token = await user.generateAuthToken();   // kullanıcı kaydolduktan sonra token veriliyor
         res.status(201).send({user, token});
     } catch (e) {
-        res.status(400).send(e);
+        res.status(400).send(e.message);
     }
 })
 
