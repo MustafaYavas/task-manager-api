478b71d3dcab85ea73ae0494777c4ec8a63165bb
Mustafa Yavaş <43928058+MustafaYavas@users.noreply.github.com>
2022-02-23 21:27:46 +0300

    removed comments

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect(process.env.MONGODB_URL , {     // veritabanına bağlanmak için
+mongoose.connect(process.env.MONGODB_URL , {   
     useNewUrlParser: true,
-    useCreateIndex: true,   // Bu, Mongoose'un MongoDB ile çalıştığı zaman, erişmemiz gereken verilere hızlı bir şekilde erişmemize olanak tanıyan indekslerimizin oluşturulduğundan emin olacak.
+    useCreateIndex: true,   
     useUnifiedTopology: true,
     useFindAndModify: false
 })
-// Mongoose veri tabanında herhangi bir collection oluşturmamama rağmen "users" adında bir collection oluşturdu. Bu Mongoose
-// tarafından oluşturduğumuz modelin ismini alıp küçük harflere dönüştürüp onu çoğul hale getirerek otomatik olarak yapar.
