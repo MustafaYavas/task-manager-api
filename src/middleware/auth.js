437e0e4b1af5589bb257c3ef560e24dbc02d0401
Mustafa Yavaş <43928058+MustafaYavas@users.noreply.github.com>
2022-02-23 21:29:36 +0300

    Removed Comments

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,17 +2,15 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user")
 
 const auth = async (req, res, next) => {
-    try {   // Bearer taşıyıcı demek. Bu sadece bir standart. "Digest", "Basic" gibi farklı kimlik doğrulama türleri de var. Ama kullanmak gerekli değil.
-        const token = req.header("Authorization").replace("Bearer ",""); // replace kullanmamızın nedeni req.header'da verdiğimiz tokenı alabilmek. req.header'daki token'ın başında Bearer ifadesi bulunuyor.
+    try {  
+        const token = req.header("Authorization").replace("Bearer ","");
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findOne({_id: decoded._id, "tokens.token": token});
-        // "tokens.token" yazımı Mongoose'da özel bir yazım. Bu yazım "tokens dizisindeki bir nesneyi arayın ve bu değere
-        // göre token özelliği olup olmadığını kontrol edin" demektir.
-        // "In the tokens array, search all the objects' token property for the following value"
+
         if(!user) 
             throw new Error();
         
-        req.token = token;  // kullanıcıya oturum açılırken verilen token (yukarıda kullanılan token). Eğer kullanıcı birden fazla tokena sahipse yalnızca birine sahip olur. (son açtığı)
+        req.token = token;  
         req.user = user;
         next();
     } catch (error) {
