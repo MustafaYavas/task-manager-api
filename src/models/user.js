2aae873bb44b48577700674ced684270816eea45
Mustafa Yavaş <43928058+MustafaYavas@users.noreply.github.com>
2022-02-23 21:31:50 +0300

    Remıved Comments

--- src/models/user.js
+++ src/models/user.js
@@ -4,13 +4,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-/* mongoose.Schema metoduyla birlikte önceden mongoose.model() metodunun 2. parametresi olarak verdiğimiz ve veritabanında collection  
-oluşturmak için kullandığımız nesneleri burada mongoose.Schema() metoduna parametre olarak veriyoruz. Bu sayede middleware olarak 
-bilinen xc şeyden faydalanabiliriz. Middleware herhangi birşey yapmadan (örn. veri kaydetme) önce veya sonra yapılacak iş olarak
-tanımlanabilir. 
-*/
-const userSchema = new mongoose.Schema({   // task-manager veri tabanında bulunan "users" collection'ınına document eklemek 
-    name: {                                // için bir nevi constructor (model denebilir) oluşturduk
+
+const userSchema = new mongoose.Schema({  
+    name: {                                
         type: String,
         required: true,
         trim: true
@@ -30,7 +26,7 @@ const userSchema = new mongoose.Schema({   // task-manager veri tabanında bulun
     age: {
         type: Number,
         default: 0,
-        validate(value){    // custom validation; kendi oluşturduğumuz kontrol yapısı
+        validate(value){    
             if (value < 0) {
                 throw new Error("Age must be greater than zero");
             }
@@ -47,8 +43,8 @@ const userSchema = new mongoose.Schema({   // task-manager veri tabanında bulun
             }
         }
     },
-    tokens: [{  // kullanıcıya verilen tokenları takip etmek için. Yani kullanıcı birden fazla yerde oturum açtığında,
-        token: {  // örneğin hem telefonda hem laptopda, ona yeni bir token verilmelidir.
+    tokens: [{  
+        token: {  
             type: String,
             required: true
         }
@@ -56,45 +52,41 @@ const userSchema = new mongoose.Schema({   // task-manager veri tabanında bulun
     avatar: {
         type: Buffer
     }
-},{ // mongoose.Schema metodunun 2. argümanı
-    timestamps: true    // default olarak false idi
+},{ 
+    timestamps: true    
 });
 
 userSchema.virtual("tasks", {
     ref: "tasks",
     localField: "_id",
-    foreignField: "owner"   // ilişki kurulmuş koleksiyondaki field'ın ismi olacak
+    foreignField: "owner"  
 })
 
-// Her nesnenin, o nesnede JSON.stringify () çağrısının çıktısının ne olacağını özelleştirmek için geçersiz kılabileceğiniz 
-// bir toJSON yöntemi vardır.
+
 userSchema.methods.toJSON = function(){ 
     const user = this;
     const userObject = user.toObject();
 
     delete userObject.password;
     delete userObject.tokens;
-    delete userObject.avatar;   // responsedan kaldırmak için yaptık
+    delete userObject.avatar;  
 
     return userObject;
 }
 
-// methods metodlarına instance metodları da denir
-// oturum açmayı başaran kullanıcıya token verilecek
+
 userSchema.methods.generateAuthToken = async function(){
-    const user = this;  // this, metodu çağıran nesnemizi işaret edecek
+    const user = this;
     const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET);
-//  ilk parametre olan {_id: user._id.toString()} tokena gömülecek olan verileri içerir. String olmalı. (payload)
-//  2. parametre ise tokenı imzalamak için kullanılır. (secret)
-    user.tokens = user.tokens.concat({token});  // yeni tokenları birleştirmek için
-    await user.save();  // token bilgilerini veri tabanına kaydetmek için
+
+    user.tokens = user.tokens.concat({token});  
+    await user.save(); 
 
     return token;
 }
 
-// statics metodlara Model metodları da denir
-// findByCredentials metodu e-postayı ve şifreyi alacak ve e-postayla bir kullanıcıyı bulmaya çalışacak ve şifreyi doğrulayacaktır.
-userSchema.statics.findByCredentials = async (email, password) => { // findByCredentials yerine başka bir isim yazılabilir. Bir middleware fonksiyonudur.
+
+userSchema.statics.findByCredentials = async (email, password) => { 
     const user = await User.findOne({email});
     if(!user){
         throw new Error("Unable to login");
@@ -105,24 +97,19 @@ userSchema.statics.findByCredentials = async (email, password) => { // findByCre
         throw new Error("Unable to login");
     }
 
-    return user;    // email ve password eşleştiyse user'ı döndürür
+    return user;  
 }
 
 // hash the plain text password before saving
 userSchema.pre("save", async function(next) {
     const user = this;
 
-    if(user.isModified("password")){    // kullanıcı parolası daha önceden hash edildi mi bunu kontrol eder. Bu blok user ilk 
-                                        // oluşturulduğunda veya güncellendiğinde çalışacak
-        user.password = await bcrypt.hash(user.password, 8);    // 8 hash algoritmasının kaç defa çalışacağını söyler
+    if(user.isModified("password")){    
+        user.password = await bcrypt.hash(user.password, 8);    
     }
 
     next();
 });   
-
-// encryption algoritmaları geri döndürelebilir. Örneğin şifremiz "Mustafa123" olsun ve bunun şifrelenmiş hali "unlkqhxaqwl0*7894"
-// olsun. Bu durumda şifrelenmiş algoritmaya ulaşan biri geri dönerek asıl şifreye ulaşmış olur.
-// Fakat hashing algoritmaları böyle değildir. Şİfrelenmiş passwordlar geri döndürülemez.
 
 
 // delete user tasks when user is removed
