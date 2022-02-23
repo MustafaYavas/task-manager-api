ddbfcbe26918f1727946dc69ab890ec232a110e9
Mustafa Yavaş <43928058+MustafaYavas@users.noreply.github.com>
2022-02-23 21:28:42 +0300

    Removed Comments

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,6 @@
-// Kaydolmak veya hesabı silmek gibi şeyler için buradan mail göndereceğiz
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);   // bu API ile çalışmak istediğimizi bildiriyoruz
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
@@ -25,18 +24,3 @@ module.exports = {
     sendWelcomeEmail,
     sendCancelationEmail
 }
-
-// const message = {   // Bu nesnenin içinde mail'in kime gideceğinden, içeriğinin e olduğundan vb. şeyler bulunur
-//     to: "mustafayavas40@gmail.com",
-//     from: "mustafayavas40@gmail.com",
-//     subject: "this is my new creation",
-//     text: "I hope this one actually get to you"
-//     // buraya html kodu da ekleyebiliriz
-// }
-
-// // send metoduna yukarıda oluşturduğumz nesneyi veriyoruz. 
-// sgMail.send(message).then(() => { 
-//     console.log("E-mail sent successfully!");
-// }).catch((error) => {
-//     console.log(error);
-// })
